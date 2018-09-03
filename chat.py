8acfbe93d04c2099b7fa5f71277c577e898657b1
msmow <masha.kondratieva@gmail.com>
2018-09-03 19:01:45 +0300

    changed the main functions of server and chat
    chat now creates both server and client and runs the server, server now connects and listens

--- chat.py
+++ chat.py
@@ -1,12 +1,18 @@
 import client
 import server
-import threading
-
 
 HOST1 = '192.68.43.238'
 PORT1 = 4000
-new_server = server.Server()
 
 HOST2 = '192.68.43.126'
 PORT2 = 4001
-new_client = client.Client()
+
+
+def main():
+	new_server = server.Server()
+	new_client = client.Client()
+	new_server.run()
+
+
+if __name__ == '__main__':
+	main()
